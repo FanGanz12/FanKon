94004044d41d0d6e2db36fecd9598fde27091976
FanGanz12 <100126026+FanGanz12@users.noreply.github.com>
2022-03-14 13:41:16 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,17 +1,15 @@
-const { tiktokdl, tiktokdlv2 } = require('@bochilteam/scraper')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    const { author: { nickname }, video, description } = await tiktokdl(args[0]).catch(async _ => await tiktokdlv2(args[0]))
-    const url = video.no_watermark || video.no_watermark_hd || video.with_watermark || video.no_watermark_raw
-    if (!url) throw 'Can\'t download video!'
-    conn.sendFile(m.chat, url, 'tiktok.mp4', `
-🔗 *Url:* ${url}
-🧏 *Nickname:* ${nickname}${description ? `🖹 *Description:* ${description}` : ''}
-`.trim(), m)
+const fetch = require('node-fetch')
+
+let handler = async (m, { conn, args }) => {
+    if (!args[0]) throw 'Uhm..urlnya mana?'
+    let res = await fetch(`https://server1.majhcc.xyz/api/tk?url=${args[0]}`)
+    if (!res.ok) throw await res.text()
+    let json = await res.json()
+    if (json.success !== true) throw json.error
+    conn.sendFile(m.chat, json.link, '', json.link, m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-
 handler.command = /^(tik(tok)?(dl)?)$/i
 
 module.exports = handler
